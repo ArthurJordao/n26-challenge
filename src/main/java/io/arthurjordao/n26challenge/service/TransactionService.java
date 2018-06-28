617a9280fe73c6905a404715c19650f028c88aef
ArthurJordao <arthurbernardijordao@gmail.com>
2018-06-27 22:32:26 -0300

    Add transaction service

---
+++ src/main/java/io/arthurjordao/n26challenge/service/TransactionService.java
@@ -0,0 +1,30 @@
+package io.arthurjordao.n26challenge.service;
+
+import io.arthurjordao.n26challenge.model.Statics;
+import io.arthurjordao.n26challenge.model.Transaction;
+import io.arthurjordao.n26challenge.repository.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+    private TransactionRepository repository;
+
+    public TransactionService() {  }
+
+    public TransactionService(TransactionRepository repository) {
+        this.repository = repository;
+    }
+
+    public boolean saveTransaction(Transaction transaction) {
+        if (transaction.isValidTransaction()) {
+            repository.save(transaction);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Statics getStatics() {
+        return Statics.from(repository.getValidTransactions());
+    }
+}
